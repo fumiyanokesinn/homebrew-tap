e5c1e3e1f54ac31f362d1ce0c31193fc672af7b1
fumiyanokesinn <shindori238@gmail.com>
2026-02-02 01:02:54 +0900

    Update formula to use prebuilt binaries

--- Formula/pomodoro-cli.rb
+++ Formula/pomodoro-cli.rb
@@ -1,14 +1,35 @@
 class PomodoroCli < Formula
   desc "CLI-based Pomodoro timer written in Go"
   homepage "https://github.com/fumiyanokesinn/pomodoro-cli"
-  url "https://github.com/fumiyanokesinn/pomodoro-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "8e66cc80a690de9994395b1c15afc726753930fad729ea2ad4fd90d6f5f0ed9a"
+  version "0.1.0"
   license "MIT"
 
-  depends_on "go" => :build
+  on_macos do
+    on_intel do
+      url "https://github.com/fumiyanokesinn/pomodoro-cli/releases/download/v0.1.0/pomodoro-cli-v0.1.0-darwin-amd64.tar.gz"
+      sha256 "811ee7a0c726769a9c1c5aa7321514a594b6045ee4f2dd181a10c3636970c9d4"
+    end
+
+    on_arm do
+      url "https://github.com/fumiyanokesinn/pomodoro-cli/releases/download/v0.1.0/pomodoro-cli-v0.1.0-darwin-arm64.tar.gz"
+      sha256 "48f8bdcd2abaf39a1dbfd6dd2049d51885792afc430bb96793bdfe74dc22862f"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/fumiyanokesinn/pomodoro-cli/releases/download/v0.1.0/pomodoro-cli-v0.1.0-linux-amd64.tar.gz"
+      sha256 "5afd53777a6ba8faeea8994aab742db2a82fbb26b570dfeb6280a268dd9bf165"
+    end
+
+    on_arm do
+      url "https://github.com/fumiyanokesinn/pomodoro-cli/releases/download/v0.1.0/pomodoro-cli-v0.1.0-linux-arm64.tar.gz"
+      sha256 "ce5b2a9a31bddef1ed18bcc29948aff413cc7d374fa806e297ed7360bfae4e3b"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/pomodoro"
+    bin.install "pomodoro-cli"
   end
 
   test do
